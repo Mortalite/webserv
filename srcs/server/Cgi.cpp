5d015f3081fbb779f4d146252e63fe1b3b0cbb28
schips <tkpokidova@gmail.com>
2021-05-14 21:36:27 +0300

    GATEWAY_INTERFACE done

--- srcs/server/Cgi.cpp
+++ srcs/server/Cgi.cpp
@@ -11,15 +11,18 @@ int Cgi::make_env_var(Response & resp){
 	std::vector<Server> serv = data->getServers();
 	std::cout << *server << std::endl;
 	//как выбрать какой сервер сейчас работает?
-	env_var["SERVER_NAME"] = serv[0]._serverName[0];
 	const Client * client = resp.getClient();
 	//тут скорее всего нужно смотреть не заголовок, а длину сообщения, проверить
-	if (client->_hdrMap.find("content-length") != client->_hdrMap.end()){
-		env_var["CONTENT_LENGTH"] = client->_hdrMap.find("content-length")->second;
-	}
 	if (client->_hdrMap.find("authorization") != client->_hdrMap.end()){
 		env_var["AUTH_TYPE"] = client->_hdrMap.find("authorization")->second;
 	}
+	if (client->_hdrMap.find("content-length") != client->_hdrMap.end()){
+		env_var["CONTENT_LENGTH"] = client->_hdrMap.find("content-length")->second;
+	}
+//	CONTENT_TYPE
+	env_var["GATEWAY_INTERFACE"] = "CGI/1.1";
+	env_var["SERVER_NAME"] = serv[0]._serverName[0];
+
 	return 0;
 }
 
