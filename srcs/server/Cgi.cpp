3479ab9ac5fe8b2a40c13b8c915d564517dd7292
schips <tkpokidova@gmail.com>
2021-05-17 16:50:40 +0300

    added PATH_INFO env var to Cgi

--- srcs/server/Cgi.cpp
+++ srcs/server/Cgi.cpp
@@ -8,20 +8,23 @@ int Cgi::make_env_var(Response & resp){
 	const Data * data = resp.getData();
 	const Server* server = resp.getClient()->_respSvr;
 
-	std::vector<Server> serv = data->getServers();
+//	std::vector<Server> serv = data->getServers();
 	std::cout << *server << std::endl;
-	//как выбрать какой сервер сейчас работает?
 	const Client * client = resp.getClient();
-	//тут скорее всего нужно смотреть не заголовок, а длину сообщения, проверить
 	if (client->_hdrMap.find("authorization") != client->_hdrMap.end()){
 		env_var["AUTH_TYPE"] = client->_hdrMap.find("authorization")->second;
 	}
+	//тут скорее всего нужно смотреть не заголовок, а длину сообщения, проверить
 	if (client->_hdrMap.find("content-length") != client->_hdrMap.end()){
 		env_var["CONTENT_LENGTH"] = client->_hdrMap.find("content-length")->second;
 	}
 //	CONTENT_TYPE
 	env_var["GATEWAY_INTERFACE"] = "CGI/1.1";
-	env_var["SERVER_NAME"] = serv[0]._serverName[0];
+	env_var["SERVER_NAME"] = server->_serverName[0];
+//	тут проверить абсолютный/относительный путь
+	if (client->_hdrMap.find("request_target") != client->_hdrMap.end()){
+		env_var["PATH_INFO"] = client->_hdrMap.find("request_target")->second;
+	}
 
 	return 0;
 }
